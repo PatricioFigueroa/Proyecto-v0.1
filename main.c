76d4ab864752e9765058e2df4d8cb8b6dd1355d1
lRaging <127540478+lRaging@users.noreply.github.com>
2023-06-27 23:08:46 +0000

    main

--- main.c
+++ main.c
@@ -18,7 +18,74 @@ typedef struct{
   int gastoDia;
 }tipoDia;
 
-int main()
-{
-  
+int main() {
+  HashMap *mapaProducto = createMap((long)100);
+  HashMap *mapaSemanal = createMap((long)100);
+  List * listaDias = createList();
+  char caracter[100], *nombre, *archivoCargado;
+  int precioCompra, precioVenta, stockInicial, antVendida, opcion, contadorDia = 0;
+  FILE *ArchivoPrueba = fopen("ArchivoPrueba.csv", "r");
+  fgets(caracter, 99, ArchivoPrueba);
+  while (fscanf(ArchivoPrueba, "%m[^,],%d,%d,%d\n", &nombre, &precioCompra, &precioVenta, &stockInicial) != EOF) {
+    tipoProducto *producto = malloc(sizeof(tipoProducto));
+    producto->nombre = strdup(nombre);
+    producto->precioCompra = precioCompra;
+    producto->precioVenta = precioVenta;
+    producto->stockInicial = stockInicial;
+    producto->cantVendida = 0;
+    insertMap(mapaProducto, producto->nombre, producto);
+    insertMap(mapaSemanal, producto->nombre, producto);
+  }
+    
+  fclose(ArchivoPrueba);
+  system("python test.py");
+  while(true)
+  {
+  printf("\n--- Menú Principal ---\n");
+  printf("1. Registro de productos\n");
+  printf("2. Control de stock\n");
+  printf("3. Modificar datos de un producto\n");
+  printf("4. Comparar y graficar dos informes semanales\n");
+  printf("5. Cargar csv de stock\n");
+  printf("6. Finalizar el día, creación de gráfico e informe\n");
+  printf("Seleccione una opción: \n");
+  scanf("%d",&opcion);
+  getchar();
+  switch (opcion) 
+    {
+      case 1:
+          registro_de_productos(mapaProducto);
+        break;
+      case 2:
+        Control_de_stock(mapaProducto);
+        break;
+      case 3:
+        modificar_datos_de_un_producto(mapaProducto);
+        break;
+      case 4: 
+        mostrarMapa(mapaProducto);
+        break;
+      case 5:
+        printf("\n————————————————————————————————————————————————————————————\n");
+        printf("Ingrese el nombre del archivo, introduzca el formato (.csv)\n");
+        scanf("%m[^\n]",&archivoCargado);
+        getchar();
+        if (strstr(archivoCargado,".csv")==0)
+        {
+          printf("\nEl formato del archivo '%s' es incorrecto.\n", archivoCargado);
+              printf("————————————————————————————————————————————————————————————\n\n");
+          break;
+        }
+        Cargar_csv_de_stock(mapaProducto, archivoCargado);
+      printf("————————————————————————————————————————————————————————————\n\n");
+        break;
+      case 6:
+        finalizarDia(mapaProducto,listaDias);
+        contadorDia++;
+        break;
+      default:
+        break;
+    }
+  }
+  return 0;
 }
---
+++ test.py
@@ -0,0 +1,17 @@
+#import matplotlib.pyplot as plt
+import pandas as pd
+
+print("Escribe algo:")
+cadena = input()
+print(cadena + " esto se imprimió desde python")
+
+# Guarda la cadena en una lista
+cadenas = [cadena]
+
+# Genera la gráfica utilizando plotline
+#longitudes = [len(c) for c in cadenas]
+#plt.plot(range(len(cadenas)), longitudes)
+#plt.xlabel('Índice de cadena')
+#plt.ylabel('Longitud de cadena')
+#plt.title('Longitud de cadenas ingresadas')
+#plt.show()
